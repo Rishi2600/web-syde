495fab897cba5f95574da24796a7bf1259c3b643
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-04-12 02:16:14 +0530

    fix tailwind default config for colors and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
       <h2 className="bg-red-300 ">HI THERE</h2>
-      <h1 className="m-2 bg-primary text-secondary hover:bg-accent p-2">HELLO</h1>
+      <h1 className="m-2 bg-primary text-secondary hover:bg-accent p-2 mt-12">HELLO</h1>
     </>
   );
 }
