39d48a4a1a35c25118f6244c2c71ff133ccc53af
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-04-12 01:02:57 +0530

    add tailwind fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <h2 className="bg-red-300 ">HI THERE</h2>
+      <h1 className="m-2 bg-primary text-secondary hover:bg-accent p-2">HELLO</h1>
     </>
   );
 }
