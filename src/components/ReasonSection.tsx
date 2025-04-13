1a827b6c82e8976355ba94671ccf2fbb32c08b7f
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-04-14 03:07:50 +0530

    FIX ReasonSection and globals.css

--- src/components/ReasonSection.tsx
+++ src/components/ReasonSection.tsx
@@ -1,10 +1,27 @@
 export function ReasonSection() {
     return (
         <>
-            <div className="py-16 relative box-border">
+            <div className="py-16 relative box-border text-secondary">
                 <div className="container">
-                    HI THERE
-                    <div> HELLO</div>
+                    <div className="transition">
+                        <div className="row">
+                            <div className="py-0 px-11 box-border ">
+                                <div className="size-7 leading-10">You may need our services if you want to:</div>
+                            </div>
+
+                            <div className="py-0 px-11 box-border ">
+                                <div className="mb-32 pl-16">
+                                    <ul className="list-none m-0 p-0 ">
+                                        <li className="mb-16 relative">
+                                            <div className="flex ">
+
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
