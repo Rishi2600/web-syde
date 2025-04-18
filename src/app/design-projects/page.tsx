91220493b9661fd4c0dd90e375a07ffa73e896b2
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-04-18 23:18:36 +0530

    add PageTitle Section

--- src/app/design-projects/page.tsx
+++ src/app/design-projects/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar";
+import PageTitle from "@/components/PageTitle";
 
 
 export default function DesignProjects() {
     return (
         <>
             <h1>Design Projects</h1>
+            <Navbar />
+            <PageTitle />
         </>
     )
 }
---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,16 @@
+export default function PageTitle() {
+    return (
+        <>
+            <div className="flex flex-col justify-center pt-24 pb-12 text-secondary">
+                <div className="container">
+                    <h1 className="text-lg mb-4 font-normal leading-normal">Projects</h1>
+                    <h2 className="text-4xl mb-8 font-medium leading-snug">
+                    Take a look at some of the ideas <br />
+                    we’ve helped to implement. <br />
+                    Will yours be next?
+                    </h2>
+                </div>
+            </div>
+        </>
+    )
+}
